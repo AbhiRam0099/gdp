f51c0205fab644897875a05813541fabed4fee5f
AbhiRam0099 <52668950+AbhiRam0099@users.noreply.github.com>
2021-10-19 23:49:05 -0500

    resolved merge conflict

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const _=require("lodash");
+const _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -11,49 +11,46 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
-<<<<<<< HEAD
 const PORT = process.env.PORT || 5000;
-=======
->>>>>>> 89b86cc44d13dc58c95b383c62cd067258cb60bb
+
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-var posts=[];
+var posts = [];
 
-app.get("/",function(req,res){
+app.get("/", function (req, res) {
 
-  res.render("home",{homeStartingContent:homeStartingContent,post:posts});
+  res.render("home", { homeStartingContent: homeStartingContent, post: posts });
 
 });
 
-app.get("/about",function(req,res){
+app.get("/about", function (req, res) {
 
-  res.render("about",{aboutContent:aboutContent});
+  res.render("about", { aboutContent: aboutContent });
 });
 
-app.get("/contact",function(req,res){
+app.get("/contact", function (req, res) {
 
-  res.render("contact",{contactContent:contactContent});
+  res.render("contact", { contactContent: contactContent });
 });
 
-app.get("/login",(req,res) =>
-    {
-      res.render("login")
-    }); 
-
-app.get("/list",(req,res) =>
-    {
-      res.render("list")
-    }); 
-
-
-
-<<<<<<< HEAD
-app.listen(PORT, function() {
-=======
-app.listen(5000, function() {
->>>>>>> 89b86cc44d13dc58c95b383c62cd067258cb60bb
-  console.log("Server started on port 5000");
+app.get("/login", (req, res) => {
+  res.render("login")
 });
+
+app.get("/list", (req, res) => {
+  res.render("list")
+});
+
+
+
+
+app.listen(PORT, function () {
+
+  app.listen(5000, function () {
+
+    console.log("Server started on port 5000");
+  });
+})
